0baee90fa8c1b22ee02bc99946bb43247ca5081f
Dominic Wokorach-O <{dominic.olanya}@mail.com>
2023-01-04 16:05:12 +0000

    Update commit

--- constants/movie.ts
+++ constants/movie.ts
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://image.tmdb.org/t/p/original/'
+export const baseUrl =
+  'https://api.themoviedb.org/3/movie/550?api_key={api_key}&language=en-US&append_to_response=images&include_image_language=en,null'
